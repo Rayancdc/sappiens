9218f26c7eb983c4bda39f0f68340e320b51d540
Rayan Castro <rayancdc@gmail.com>
2018-08-06 21:45:24 -0300

    Add pages,Users and Experiences controller

---
+++ app/controllers/experiences_controller.rb
@@ -0,0 +1,51 @@
+class ExperiencesController < ApplicationController
+  before_action :set_experience, only: [:show, :edit, :update, :destroy]
+  skip_before_action :authenticate_user!
+
+  def dashboard
+  end
+
+  def index
+    @experiences = Experience.all
+  end
+
+  def show
+  end
+
+  def new
+    @experience = Experience.new
+  end
+
+  def create
+    @user = current_user
+    @experience = @user.experiences.build(experience_params)
+    if @experience.save
+      redirect_to experience_path(@experience)
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    @experience.update(experience_params)
+
+    redirect_to experience_path(@experience)
+  end
+
+  def destroy
+    @experience.destroy
+    redirect_to experiences_path
+  end
+
+  private
+  def set_experience
+    @experience = Experience.find(params[:id])
+  end
+
+  def experience_params
+    params.require(:experience).permit()
+  end
+end
---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,20 @@
+class UsersController < ApplicationController
+  layout "dashboard"
+  before_action :set_user
+  def dashboard
+  end
+
+  def my_experiences
+  end
+
+  def edit_profile
+  end
+
+  def invite_friends
+  end
+
+  private
+  def set_user
+    @user = current_user
+  end
+end
