f2296300b34e5bbe9de9325c0598a466c29e5b7d
Rayan Castro <rayancdc@gmail.com>
2018-08-07 16:00:23 -0300

    Add validations and relations between models

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,2 +1,8 @@
 class Address < ApplicationRecord
+  has_many :companies, dependent: :destroy
+
+  validates :field_1, presence: true
+  validates :city, presence: true
+  validates :state, presence: true
+  validates :zipcode, presence: true
 end
