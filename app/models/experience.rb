a196c7950523fc73fa92d2742edd3e3cb5afc789
Fernando Brustolini <fbrustolini@gmail.com>
2018-08-08 16:22:57 -0300

    add backend searching through association

--- app/models/experience.rb
+++ app/models/experience.rb
@@ -1,4 +1,5 @@
 class Experience < ApplicationRecord
+
   belongs_to :company
   has_many :bookings, dependent: :destroy
   has_many :users, through: :bookings
@@ -7,4 +8,15 @@ class Experience < ApplicationRecord
   validates :description, presence: true
   validates :date, presence: true
   validates :price_cents, presence: true, numericality: { only_integer: true }
+
+    include PgSearch
+  pg_search_scope :global_search,
+    against: [ :name, :description ],
+    associated_against: {
+    	company: [ :name ]
+    },
+    using: {
+      tsearch: { prefix: true } # <-- now `product manag` will return something!
+    }
 end
+
