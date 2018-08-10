c4986a7799eab5f17becbbffe3e29e96f92818d9
Rayan Castro <rayancdc@gmail.com>
2018-08-10 17:01:46 -0300

    Add new images, geocoding map with API key, map JS pack function, improve style for navbar,sidebar and banner

--- app/javascript/packs/map.js
+++ app/javascript/packs/map.js
@@ -2,11 +2,11 @@ import GMaps from 'gmaps/gmaps.js';
 
 const mapElement = document.getElementById('map');
 if (mapElement) { // don't try to build a map if there's no div#map to inject in
-  const map = new GMaps({ el: '#map', lat: 0, lng: 0 });
+  const map = new GMaps({ el: '#map', lat: -23.554644, lng: -46.6637513 });
   const markers = JSON.parse(mapElement.dataset.markers);
   map.addMarkers(markers);
   if (markers.length === 0) {
-    map.setZoom(2);
+    map.setZoom(8);
   } else if (markers.length === 1) {
     map.setCenter(markers[0].lat, markers[0].lng);
     map.setZoom(14);
