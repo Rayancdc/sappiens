97745e780af0ccef426c330ada20e1bf07b69ee0
Rayan Castro <rayancdc@gmail.com>
2018-08-12 18:55:39 -0300

    Start webpacker fix

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,11 +1,21 @@
+/* eslint no-console:0 */
+// This file is automatically compiled by Webpack, along with any other files
+// present in this directory. You're encouraged to place your actual application logic in
+// a relevant structure within app/javascript and only use these pack files to reference
+// that code so it'll be compiled.
+//
+// To reference this file, add <%= javascript_pack_tag 'application' %> to the appropriate
+// layout file, like app/views/layouts/application.html.erb
+
+console.log('Hello World from Webpacker')
+
 import 'bootstrap';
 import { initUpdateNavbarOnScroll } from '../components/navbar';
 import { loadDynamicBannerText } from '../components/banner';
 import { setStickyFooter } from '../components/footer';
 import { autocomplete } from '../components/autocomplete';
-import { setCalendar } from '../components/calendar';
-
-console.log("hello from webpacker");
+import { setCalendar } from '../components/calendar'; c
+onsole.log("hello from webpacker");
 
 setCalendar();
 
