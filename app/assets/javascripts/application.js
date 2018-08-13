1c2a4120862eb6bc5f47dd9109e70aa3a4f4b285
Rayan Castro <rayancdc@gmail.com>
2018-08-13 02:30:43 -0300

    Add working calendar only showing event dates for selection

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -3,7 +3,40 @@
 //= require rails-ujs
 //= require_tree .
 
-const setCalendar = (events) => {
-  console.log(events);
-  $('#calendar').datepicker();
+const compareDates = (date1, date2) => {
+    return (date1.getFullYear() === date2.getFullYear()) &&
+           (date1.getMonth() === date2.getMonth()) &&
+           (date1.getDate() == date2.getDate());
 }
+
+const compareArray = (events, date) => {
+  let check = false;
+  events.forEach((event) => {
+    if (compareDates(event, date)) {
+      check = true;
+    }
+  })
+  return check;
+}
+
+const setCalendar = (events) => {
+  console.log(events)
+  $('#calendar').datepicker({
+    onRenderCell: function(date, cellType) {
+      if (cellType == 'day') {
+        let isEnabled = compareArray(events, date)
+
+        return {
+          disabled: !isEnabled
+        }
+      }
+    }
+  });
+}
+
+const gotoEvent = (event) => {
+  $('#calendar').data('datepicker').selectedDates
+}
+
+
+$("#book-button").addEventListener("click", gotoEvent)
