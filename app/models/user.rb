2de5a548cc1a9fd2ad94765f06687dd42405082a
Rayan Castro <rayancdc@gmail.com>
2018-08-16 22:14:55 -0300

    Fix facebook image bug

--- app/models/user.rb
+++ app/models/user.rb
@@ -22,7 +22,7 @@ class User < ApplicationRecord
     user = where(email: auth.info.email).first_or_initialize do |user|
       user.password = Devise.friendly_token[0,20]
       user.name = auth.info.name   # assuming the user model has a name
-      user.image = auth.info.image # assuming the user model has an image
+      user.remote_image_url = auth.info.image # assuming the user model has an image
     end
     if user.provider.nil? || user.uid.nil?
       user.provider = auth.provider
